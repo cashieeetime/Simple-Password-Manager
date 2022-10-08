2f5642b17d2a3ebd8c55c01a20b8f53046f12c7d
cashieeetime <cashelcreates@gmail.com>
2022-10-08 00:17:44 -0400

    guide update, added function to read/build dictionary

--- passwords.py
+++ passwords.py
@@ -1,9 +1,9 @@
-use_hardcoded_filename = False
+use_hardcoded_filename = True
 
 def open_file ():
     '''repeatedly prompts for a file until one is successfully opened'''
     if use_hardcoded_filename:
-        file = open("cashie-passwords.txt")
+        file = open("c-passwords.txt")
         return file
     else:
         while True:
@@ -13,17 +13,19 @@ def open_file ():
                 return file
             except FileNotFoundError:
                 print("Error: File not found.")
-                
                 continue 
 
 def read_file (file):
     ''' Reads the file and separates lines, then calls a function to form the dictionary'''
     data_dict = {}
-    
-    file.readline()
-    for line in file:
-        print(line)
-                    
+    next(file)
+    next(file)
+    for line in file:   
+        line = line.strip().replace(" ", "")
+        line_list = line.split("|")
+        linelist = [line_list[1], line_list[2], line_list[3]]        
+        site = line_list[0]
+        data_dict[site] = linelist        
     return data_dict
     
 
@@ -32,6 +34,8 @@ def read_file (file):
 def main ():
     file = open_file()
     read_file(file)
+    
+    file.close()
     
 
 main()
