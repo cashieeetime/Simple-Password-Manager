f040311564d9789b4854d0c58c86d45857a6ab68
cashieeetime <cashelcreates@gmail.com>
2022-10-25 15:11:42 -0400

    finished add_entry()

--- passwords.py
+++ passwords.py
@@ -4,15 +4,14 @@ use_hardcoded_filename = True
 def initialize_file ():
     '''repeatedly prompts for a file until one is successfully opened'''
     if use_hardcoded_filename:
-        file = open("example_password_format.txt")
         fp = "example_password_format.txt"
-        return file, fp
+        return fp
     else:
         while True:
             fp = input("\nEnter a file name: ")
             try:
                 file = open(fp, "r")
-                return file, fp
+                return fp
             except FileNotFoundError:
                 print("\n    Error: File not found.")
                 continue 
@@ -21,9 +20,9 @@ def read_file (fp):
     file = open(fp, "r")
     return file
 
-def append_file (fp):
-    file = open(fp, "a")
-    file.close()
+def write_file (fp):
+    file = open(fp, "w")
+    return file
 
 def build_dict (file):
     '''Reads the file and splits lines, then calls a function to form the dictionary'''
@@ -101,87 +100,93 @@ def p_lookup (data_dict, choice):
         print("We couldn't find any websites with that name. Do any of these similar websties match what you are looking for?\n")
         return alt_output
 
-def add_entry (file, website, email, username, password):
+def add_entry (file, data_dict, website, email, username, password):
     '''add an entry to the original file'''
+    linelist = [email, username, password]
+    data_dict[website] = linelist
     spacer = " | "
-    string = website + spacer + email + spacer + username + spacer + password
-    file.write(string) 
-
-    '''append newline of data to dictionary then overwrite whole file?'''
+    for key in  data_dict:
+        file.write(key + spacer + data_dict[key][0] + spacer + data_dict[key][1] + spacer + data_dict[key][2] + "\n")
+    file.close()
 
 def edit_entry(file):
     '''edit an already existing entry in the file'''
     pass
 
-def main ():
-    print("\nHello. Welcome to the password manager. To get started, we need the name of the file that is holding your data.\n")
-    file, fp = initialize_file()
-    data_dict = build_dict(file)
-    w, e, u, p = count_char(data_dict)
-    cont = True
 
-    while cont:
-        q1_repeat = True
-        q2_repeat = True
-        while q1_repeat == True:
-            print("What would you like to do today?\n    1. View a complete, alphabetized list of websites, emails and passwords.\n    2. View an alphabetized list of just the websites.\n    3. Look up login information for a specific website.\n    4. Add and save a new entry to your data.\n    5. Edit an already existing entry in the database.")
-            answer = str(input("-> "))
+
+
+
+print("\nHello. Welcome to the password manager. To get started, we need the name of the file that is holding your data.\n")
+
+cont = True
+while cont:
+    q1_repeat = True
+    q2_repeat = True
+    while q1_repeat == True:
+        fp = initialize_file()
+        file = read_file(fp)
+        data_dict = build_dict(file)
+        w, e, u, p = count_char(data_dict)
+        print("What would you like to do today?\n    1. View a complete, alphabetized list of websites, emails and passwords.\n    2. View an alphabetized list of just the websites.\n    3. Look up login information for a specific website.\n    4. Add and save a new entry to your data.\n    5. Edit an already existing entry in the database.")
+        answer = str(input("-> "))
+        print()
+
+        if answer.strip() == "1":
+            output = print_all(data_dict)
+            pretty_print(output, w, e, u, p)
+            q1_repeat = False
+            
+        elif answer.strip() == "2":
+            print_sites(data_dict)
             print()
+            q1_repeat = False
+            
+        elif answer.strip() == "3":
+            print("What website would you like to search for?")
+            choice = input("-> ")
+            print()
+            output = p_lookup(data_dict, choice)
+            pretty_print(output, w, e, u, p)
+            q1_repeat = False
+            
+        elif answer.strip() == "4":
+            print("We will now ask you to input the necessary login information. Type \"n/a\" for any fields you'd like to leave blank.\n")
+            website = input("What is the name of the website? \n    -> ")
+            email = input("What is the email address? \n    -> ")
+            username = input("What is the username? \n    -> ")
+            password = input("What is the password? \n    -> ")
+            file.close()
+            file = write_file(fp)
+            add_entry(file, data_dict, website, email, username, password)
+            print("\nYour data has been sucessfully saved to your file.")
+            q1_repeat = False
+            
+        elif answer.strip() == "5":
+            edit_entry(file)
+            print("This function hasn't been defined yet.")
+            q1_repeat = False
+            
+        else:
+            print("I didn't understand that.\n")
+            q1_repeat = True
 
-            if answer.strip() == "1":
-                output = print_all(data_dict)
-                pretty_print(output, w, e, u, p)
-                q1_repeat = False
-            
-            elif answer.strip() == "2":
-                print_sites(data_dict)
-                q1_repeat = False
-            
-            elif answer.strip() == "3":
-                print("What website would you like to search for?")
-                choice = input("-> ")
-                print()
-                output = p_lookup(data_dict, choice)
-                pretty_print(output, w, e, u, p)
-                q1_repeat = False
-            
-            elif answer.strip() == "4":
-                print("We will now ask you to input the necessary login information. Type \"n/a\" for any fields you'd like to leave blank.")
-                website = input("What is the name of the website? \n-> ")
-                email = input("What is the email address? \n-> ")
-                username = input("What is the username? \n-> ")
-                password = input("What is the password? \n-> ")
-                append_file(fp)
-                add_entry(file, website, email, username, password)
-                #print("\nYour data has been sucessfully saved to your file.")
-                q1_repeat = False
-            
-            elif answer.strip() == "5":
-                # edit_entry(file)
-                print("This function hasn't been defined yet.")
-                q1_repeat = False
-            
-            else:
-                print("I didn't understand that.\n")
-
-        while q2_repeat:
-            print("\nWould you like to start over? (yes/no)")
-            answer = str(input("-> "))
         
-            if answer.lower() == "no":
-                cont = False
-                print("\nThank you for using the password manager. We will now close the program.\n")
-                file.close()
-                q2_repeat = False
+    while q2_repeat:
+        print("\nWould you like to start over? (yes/no)")
+        answer = str(input("-> "))
         
-            elif answer.lower() == "yes":
-                read_file(fp)
-                print()
-                q1_repeat = True
-                q2_repeat = False
+        if answer.lower() == "no":
+            cont = False
+            print("\nThank you for using the password manager. We will now close the program.\n")
+            q2_repeat = False
         
-            else:
-                print("\nI didn't understand that.")
-                q2_repeat = True
-
-main()
+        elif answer.lower() == "yes":
+            read_file(fp)
+            print()
+            q1_repeat = True
+            q2_repeat = False
+        
+        else:
+            print("\nI didn't understand that.")
+            q2_repeat = True
