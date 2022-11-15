ee2b34263a40f366c02c97551e2fc4bb2d8364bf
cashieeetime <cashelcreates@gmail.com>
2022-11-14 19:39:42 -0500

    finished edit_entry() and minor errors

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from asyncore import read
-
 #
-# debugging functions
+# DEBUGGING FUNCTIONS
 #
 def default_fp ():
     '''default file for debugging purposes; can also be used by the user for their own file'''
@@ -9,15 +8,15 @@ def default_fp ():
     return fp
 
 #
-# file functions
+# FILE FUNCTIONS
 #
 def initialize_file ():
     '''repeatedly prompts for a file until one is successfully opened'''
     while True:
-        fp = input("\nEnter a file name: ")
+        fp = input("Enter a file name. \n-> ")
         try:
             file = open(fp, "r")
-            return file
+            return fp
         except FileNotFoundError:
             print("\n    Error: File not found.")
             continue 
@@ -27,16 +26,21 @@ def read_file (fp):
     file = open(fp, "r")
     return file
 
-def write_file (fp):
-    '''opens file for writing'''
+def append_file (fp):
+    '''opens file for appending'''
     file = open(fp, "a")
+    return file
+
+def write_file(fp):
+    '''opens file for writing'''
+    file = open(fp, "w")
     return file
 
 def unknown_input():
     print("I didn't understand that.\n")
 
 #
-# basic dictionary and text format functions
+# INITIALIZE DICTIONARY
 #
 def build_dict (fp):
     '''Reads the file and splits lines, then calls a function to form the dictionary'''
@@ -45,15 +49,15 @@ def build_dict (fp):
     data_dict = {}
     for line in file:   
         line = line.strip().replace(" ", "")
-        line_list = line.split("|")
-        linelist = [line_list[1], line_list[2], line_list[3]]        
-        site = line_list[0]
-        data_dict[site] = linelist
-
+        linelist = line.split("|")
+        data_dict[linelist[0]] = (linelist[1], linelist[2], linelist[3])
     file.close()
     return data_dict
 
-def count_char (data_dict):
+#
+# TEXT FORMAT FUNCTIONS
+#
+def character_count (data_dict):
     '''loops through the file to find the keyword with the largest number of characters per  category, then returns those number'''
     w = 0     #len for websites
     e = 0     #len for emails
@@ -78,7 +82,7 @@ def count_char (data_dict):
     return w, e, u, p
 
 def pretty_print (output, data_dict):
-    w, e, u, p = count_char(data_dict)
+    w, e, u, p = character_count(data_dict)
 
     '''takes output from a function and formats the result for printing'''
     print("    {:{w}s} | {:{e}s} | {:{u}s} | {:{p}s}".format("Website", "Email", "Username", "Password", w = w, e = e, u = u, p = p))
@@ -88,7 +92,7 @@ def pretty_print (output, data_dict):
         print("    {:{w}s} | {:{e}s} | {:{u}s} | {:{p}s}".format(key, output[key][0], output[key][1], output[key][2], w = w, e = e, u = u, p = p))
 
 #
-# program action functions
+# PROGRAM ACTION FUNCTIONS
 #
 def print_all (data_dict):
     '''sorts dictionary keys and stores it oin a new variable to return'''
@@ -105,133 +109,156 @@ def print_sites (data_dict):
         print("   ", key)
     print()
 
-def p_lookup (data_dict, choice):
+def p_lookup (data_dict):
     '''takes a website name as an argument, and searches the dictionary for that key; if unable to find key, function suggests related websites'''
-    choice = input("What website would you like to search for?\n-> ")
+    answer = input("What website would you like to search for?\n-> ")
     output = {}
     alt_output = {}
     for key in sorted(data_dict):
-        if choice.lower() == key.lower():
+        if answer.lower() == key.lower():
             linelist = [data_dict[key][0], data_dict[key][1], data_dict[key][2]]
             output[key] = linelist
 
-        if key.lower().startswith(choice.lower()):
+        if key.lower().startswith(answer.lower()):
             linelist = [data_dict[key][0], data_dict[key][1], data_dict[key][2]]
             alt_output[key] = linelist
 
     if output != {}:
         pretty_print(output, data_dict)
     else:
-        print("We couldn't find any websites with that name. Do any of these similar websties match what you are looking for?\n")
+        print("\nWe couldn't find any websites with that name. Do any of these similar websties match what you are looking for?\n")
         pretty_print(alt_output, data_dict)
 
 def add_entry (fp, data_dict):
     '''add an entry to the original file'''
+    file = append_file(fp)
+
     print("We will now ask you to input the necessary login information. Type \"n/a\" for any fields you'd like to leave blank.\n")
     website = input("What is the name of the website? \n-> ")
-    email = input("What is the email address? \n-> ")
-    username = input("What is the username? \n-> ")
-    password = input("What is the password? \n-> ")
-
-    file = write_file(fp)
+    email = input("\nWhat is the email address? \n-> ")
+    username = input("\nWhat is the username? \n-> ")
+    password = input("\nWhat is the password? \n-> ")
 
     linelist = [email, username, password]
     data_dict[website] = linelist
-    spacer = " | "
-    file.write("\n" + website + spacer + email + spacer + username + spacer + password)
+    file.write("\n" + website + " | " + email + " | " + username + " | " + password)
     file.close()
     print("\nYour data has been sucessfully saved to your file.")
 
-def edit_entry(file):
+def edit_entry(fp, data_dict):
     '''edit an already existing entry in the file'''
-    print("This function hasn't been defined yet.")
-
-#
-# program base
-#
-def program_intro():
-    print("\nHello. Welcome to the password manager.\n")
+    file = write_file(fp)
+    print("We will now ask you to input the necessary login information. Type \"n/a\" for any fields you'd like to leave blank.\n")
 
     while True:
-        
-        answer = input("To get started, we need the name of the file that is holding your data. Would you like to:\n    1. Provide the name of an already exisiting text file with your data\n    2. Create a new text file to store your data\n    3. Use the default file name stored in the program\n-> ")
-        print()
+        website = input("What is the name of the website you need to edit? \n-> ")
+        try:
+            linelist = data_dict[website]
+            break
+        except KeyError:
+            print("We couldn't find that website within our data.\n")
+            continue
+
+    while True:
+        answer = input("What would you like to edit?\n    1. Email Address\n    2. Username\n    3. Password\n-> ")
 
         if answer.strip() == "1":
-            fp = initialize_file()
-            return fp
-        
+            email = input("\nWhat is the new email address? \n-> ")
+            data_dict[website][0] = email
+            break
         elif answer.strip() == "2":
-            fp = str(input("What would you like your file to be called? Please include the \".txt\" in your file name.\n-> "))
-            open(fp, "r")
-            return fp
-        
+            username = input("\nWhat is the username? \n-> ")
+            data_dict[website][1] = username
+            break
         elif answer.lower() == "3":
-            fp = default_fp()
-            return fp
-
+            password = input("\nWhat is the password? \n-> ")
+            data_dict[website][2] = password
+            break
         else:
             unknown_input()
             continue
 
-def question_one(fp):
+    for key in data_dict:    
+        file.write(key + " | " + data_dict[key][0] + " | " + data_dict[key][1] + " | " + data_dict[key][2] + "\n")
+    file.close()
+    print("\nYour data has been sucessfully saved to your file.")
+            
+
+#
+# PROGRAM PARTS
+#
+def intro():
+    '''program intro to determine what file to use'''
+    print("\nHello. Welcome to the password manager.")
+
     while True:
-    
+        answer = input("\nTo get started, we need the name of the file that is holding your data. Would you like to:\n    1. Provide the name of an already exisiting text file with your data\n    2. Create a new text file to store your data\n    3. Use the default file name stored in the program\n-> ")
+        print()
+
+        if answer.strip() == "1":
+            fp = initialize_file()
+            print()
+            return fp
+        elif answer.strip() == "2":
+            fp = str(input("What would you like your file to be called? Please include the \".txt\" in your file name.\n-> "))
+            open(fp, "r")
+            return fp
+        elif answer.lower() == "3":
+            fp = default_fp()
+            return fp
+        else:
+            unknown_input()
+            continue
+
+def main_program(fp):
+    '''main program and it's functions'''
+    while True:
         data_dict = build_dict(fp) 
         answer = input("What would you like to do today?\n    1. View a complete, alphabetized list of websites, emails and passwords.\n    2. View an alphabetized list of just the websites.\n    3. Look up login information for a specific website.\n    4. Add and save a new entry to your data.\n    5. Edit an already existing entry in the database.\n-> ")
         print()
 
         if answer.strip() == "1":
             print_all(data_dict)
-            break
-            
+            break   
         elif answer.strip() == "2":
             print_sites(data_dict)
-            break
-            
+            break  
         elif answer.strip() == "3":
-            print()
             p_lookup(data_dict)
             break
-            
         elif answer.strip() == "4":
             add_entry(fp, data_dict)
             break
-
         elif answer.strip() == "5":
             edit_entry(fp, data_dict)
             break
-              
         else:
             unknown_input()
             continue
 
-
 def cont_question():
+    '''question to ask the user if they'd like to coontinue or not'''
     while True:
-
         answer = input("\nWould you like to start over? (yes/no)\n-> ")
         
         if answer.lower() == "no":
             print("\nThank you for using the password manager. We will now close the program.\n")
             return False
-        
         elif answer.lower() == "yes":
             print()
             return True
-        
         else:
             unknown_input()
             continue
 
 #
-# main
+# MAIN
 #
 def main():
     cont = True
-    program_intro()
+    fp = intro()
     while cont == True:
-        question_one()
+        main_program(fp)
         cont = cont_question()
 
 main()
